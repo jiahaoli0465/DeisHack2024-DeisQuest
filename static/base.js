30a68a3cb5259af96c693ac3986e222402b783bb
JeffreyWangg <jwang@jeffreys-mbp.dyn.brandeis.edu>
2024-01-27 02:43:08 -0500

    commit

--- static/base.js
+++ static/base.js
@@ -38,3 +38,21 @@ menu.addEventListener('mouseleave', () => {
         menu.classList.add('hidden');
     }, 300);
 });
+
+window.addEventListener('DOMContentLoaded', event => {
+        console.log("copied code")
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
